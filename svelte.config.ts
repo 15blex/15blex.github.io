8b195ccc47d5be74868947fe008a26c050bfae03
Alexi <15blex@gmail.com>
2025-11-24 07:02:02 +0100

    chore: minify build

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,10 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 
 export default {
+  compileOptions: {
+    minify: true
+  },
   kit: {
     adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/15blex.github.io' : '',
-    },
-  },
+      base: process.env.NODE_ENV === 'production' ? '/15blex.github.io' : ''
+    }
+  }
 };
